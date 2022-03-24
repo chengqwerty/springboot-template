804033f04eac60970d5b2ff0a63c1988e2b39e46
chengc <chengcheng@3ctech.cn>
2022-03-24 10:31:15 +0800

    init

---
+++ src/main/java/som/make/log/LogbackTemplateApplication.java
@@ -0,0 +1,13 @@
+package som.make.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/som/make/log/log/LogbackTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package som.make.log.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
